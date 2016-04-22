6cb054c4da1b6c38801f98e9e22ec5c410410e7b
kmhssu <kmh@ssu.ac.kr>
2016-04-22 09:42:19 +0900

    Update hello.c
    check printf function.

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <studio.h>
 int main(void){
-   printf("Hello world\n");
+    int cnt = 0;
+
+    cnt = printf("Hello world\n");
+
+    if (cnt == 12)
+       printf("ok\n");
+    else 
+       printf("something wrong\n");
+   
    return 0;
 }
